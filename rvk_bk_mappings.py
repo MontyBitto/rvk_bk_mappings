28a54c9fd9764f859cc131ca6e6471a9631b37ae
MontyBitto <160478746+MontyBitto@users.noreply.github.com>
2024-05-29 15:42:36 +0200

    Update rvk_bk_mappings.py

--- rvk_bk_mappings.py
+++ rvk_bk_mappings.py
@@ -72,43 +72,75 @@ def replace_characters(input_string): #RVK Notation für die API nutzbar machen
  replaced_string = input_string.replace(",", "%2C").replace(" ", "%2520")
  return replaced_string
 
-def rvk_bk_process(rk_data, mapping_data):
+def rvk_bk_process(rk_data, mapping_data, ext_mapping_dict, c = 0):
  for item in rk_data: # Schleife durch die Konzepte
   notation = item['notation'][0] # nächst tiefere Notation
   if "http://rdf-vocabulary.ddialliance.org/xkos#CombinedConcept" in item["type"]:
-   type = "CombinedConcept"
+   type = "combined"
   else:
-   type = "coreConcept"
+   type = ""
   if notation not in mapping_dict: # Notation hat noch kein Mapping
-   mapping_data.append([notation, type])
+   if ext_mapping_dict:
+    for bk_not, bk in ext_mapping_dict.items():
+     bk_notation = bk_not
+    for relation_mapping in bk:
+     print(relation_mapping)
+     relation = relation_mapping[0]
+     mapping_uri = relation_mapping[1]
+     mapping_data.append([notation, type, bk_notation, relation, mapping_uri])
+   else: 
+    mapping_data.append([notation, type])
   else:
    for bk_not, bk in mapping_dict[notation].items():
-    bk_notation = bk_not
+    bk_notation = bk_not         
     for relation_mapping in bk:
      relation = relation_mapping[0]
      mapping_uri = relation_mapping[1]
-     mapping_data.append([notation, type, bk_notation, relation, mapping_uri])      
+     mapping_data.append([notation, type, bk_notation, relation, mapping_uri])
+    if bk_notation in bk_narrowest and not ext_mapping_dict:
+     c = 0
+     if bk_notation in ext_mapping_dict:
+      ext_mapping_dict[bk_notation].append([relation, mapping_uri])
+     else:
+      ext_mapping_dict[bk_notation] = [[relation, mapping_uri]] 
+ 
   url_notation = replace_characters(notation)
   if url_notation:
-   rvk_bk(url_notation, mapping_data) #Rekursion für alle Unterbegruppen
+   c += 1
+   print(c, ext_mapping_dict)
+   rvk_bk(url_notation, mapping_data, ext_mapping_dict, c) #Rekursion für alle Unterbegruppen
+   c -= 1
+   if c == 0:
+    ext_mapping_dict.clear() 
  return mapping_data
 
-def rvk_bk(url_notation, mapping_data):
+def rvk_bk(url_notation, mapping_data, ext_mapping_dict, c):
  rk = requests.get('https://coli-conc.gbv.de/rvk/api/narrower?uri=http%3A%2F%2Frvk.uni-regensburg.de%2Fnt%2F'+url_notation) #Unterbegruppen
  rk_data = rk.json()
- rvk_bk_process(rk_data, mapping_data)
+ rvk_bk_process(rk_data, mapping_data, ext_mapping_dict, c)
 
 def start():
  get_current_mappings()
+ ext_mapping_dict = {}
  mapping_data = []
- rk = requests.get('https://coli-conc.gbv.de/api/voc/top?uri=http://uri.gbv.de/terminology/rvk/') #Hauptgruppen
+ if args.notation is None:
+  rk = requests.get('https://coli-conc.gbv.de/api/voc/top?uri=http://uri.gbv.de/terminology/rvk/') #Hauptgruppen
+ else: 
+  notation = args.notation
+  url_notation = replace_characters(notation)
+  rk = requests.get('https://coli-conc.gbv.de/rvk/api/narrower?uri=http%3A%2F%2Frvk.uni-regensburg.de%2Fnt%2F'+ url_notation)
  rk_data = rk.json()
- rvk_bk_process(rk_data, mapping_data)
+ rvk_bk_process(rk_data, mapping_data, ext_mapping_dict)
  return mapping_data
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-n', '--notation', dest="notation")
+args = parser.parse_args()
+
+with open('bk_narrowest.csv', 'r') as file: #Datei mit einer Liste der niedrigsten Untergruppen in der BK
+ bk_narrowest = file.read()
 
 mapping_data = start()
-mapping_data.sort(key=lambda x: x[0])
 
 with open('rvk_bk_mappings.tsv', mode='w', newline='', encoding='utf-8') as file:
  writer = csv.writer(file, delimiter='\t')
